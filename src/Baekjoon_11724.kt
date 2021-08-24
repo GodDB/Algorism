c1f673f73d3f0ccc72c628624f71713997d85178
GodDB <saqwzx002@naver.com>
2021-08-24 20:28:21 +0900

    백준 11724 문제 풀이 완료

--- src/Baekjoon_11724.kt
+++ src/Baekjoon_11724.kt
@@ -11,7 +11,7 @@ fun main() {
 
     val arr: MutableList<MutableList<Int>> = MutableList(n + 1) { mutableListOf() }
     val visitList: MutableList<Boolean> = MutableList(n + 1) { false }
-    val resultList : MutableList<Int> = MutableList(n+1) {0 }
+    val resultList: MutableList<Int> = MutableList(n + 1) { 0 }
 
     for (i in 0 until m) {
         val startNode = scan.nextInt()
@@ -25,10 +25,21 @@ fun main() {
         dfs(arr, visitList, resultList, i, i)
     }
 
-    println("${resultList.sum()}")
+    // 노드간 연결이 없는 노드도 하나의 연결요소로 보기 때문에, 각 노드에 연결이 없는 노드에 대해서도 갯수를 추가 해준다.
+    var sum = resultList.sum()
+    for( i in 1 .. n ) {
+        if(arr[i].size == 0) sum++
+    }
+    println("$sum")
 }
 
-fun dfs(arr: List<List<Int>>, visitList: MutableList<Boolean>, resultList : MutableList<Int>, startIndex: Int, targetIndex : Int) {
+fun dfs(
+    arr: List<List<Int>>,
+    visitList: MutableList<Boolean>,
+    resultList: MutableList<Int>,
+    startIndex: Int,
+    targetIndex: Int
+) {
     visitList[startIndex] = true
 
     for (node in arr[startIndex]) {

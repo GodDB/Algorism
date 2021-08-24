9aa7382bf52b046d8f58ff21fd3f6659713cb4aa
dongbeombae <dongbeom_bae@mycelebs.com>
2021-08-24 16:57:05 +0900

    백준 1260번 풀이완료

---
+++ src/Baekjoon_1260.kt
@@ -0,0 +1,61 @@
+import java.util.*
+
+lateinit var visitList : MutableList<Boolean>
+
+fun main() {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextInt()
+    val m = scan.nextInt()
+    val k = scan.nextInt()
+
+    val arr: MutableList<MutableList<Int>> = MutableList(n + 1) { mutableListOf() }
+    visitList = MutableList(n+1) { false }
+
+    for (i in 0 until m) {
+        val startNode = scan.nextInt()
+        val endNode = scan.nextInt()
+
+        arr[startNode].add(endNode)
+        arr[endNode].add(startNode)
+    }
+
+    for (nodeList in arr) {
+        nodeList.sortBy { it }
+    }
+
+    dfs(arr, k)
+    println()
+    bfs(arr, k)
+}
+
+
+fun dfs(arr: List<List<Int>>, startIndex: Int) {
+    visitList[startIndex] = true
+    print("$startIndex ")
+
+    for (node in arr[startIndex]) {
+        if(!visitList[node]){
+            dfs(arr, node)
+        }
+    }
+}
+
+fun bfs(arr : List<List<Int>>, startIndex: Int) {
+
+    val queue : Queue<Int> = LinkedList()
+    queue.add(startIndex)
+    print("$startIndex ")
+    visitList[startIndex] = false
+
+    while(queue.isNotEmpty()){
+        val index = queue.poll()
+        for( value in arr[index] ){
+            if(visitList[value]) {
+                visitList[value] = false
+                print("$value ")
+                queue.add(value)
+            }
+        }
+    }
+}

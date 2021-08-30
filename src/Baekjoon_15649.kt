9243b7a5182e796533974de0d2084d8b4101e37e
GodDB <saqwzx002@naver.com>
2021-08-30 20:12:21 +0900

    백준 15649 문제 풀이 완료

--- src/Baekjoon_15649.kt
+++ src/Baekjoon_15649.kt
@@ -1,26 +1,32 @@
 import java.util.*
-import kotlin.math.max
 
+/** https://www.acmicpc.net/problem/15649 */
 fun main() {
     val scan = Scanner(System.`in`)
     val n = scan.nextInt()
     val m = scan.nextInt()
 
-    val arr = MutableList(n) { 0 }
-    backTracking(0, n, arr)
+    val arr = MutableList(m) { 0 }
+    val check = MutableList(n) { false }
+    backTracking(0, m, n, arr, check)
 
 }
 
-private fun backTracking(index: Int, maxIndex: Int, arr: MutableList<Int>) {
-    if (index == maxIndex) {
-        for( i in 0 until index) {
+private fun backTracking(index: Int, m: Int, n: Int, arr: MutableList<Int>, check: MutableList<Boolean>) {
+    if (index == m) {
+        for (i in 0 until index) {
             print("${arr[i]} ")
         }
         println()
+        return
     }
 
-    for( i in index until maxIndex) {
-        arr[index] = i
-        backTracking(index + 1, maxIndex, arr)
+    for (i in 0 until n) {
+        if (check[i]) continue
+
+        check[i] = true
+        arr[index] = i + 1
+        backTracking(index + 1, m, n, arr, check)
+        check[i] = false
     }
 }

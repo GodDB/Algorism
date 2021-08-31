24d9b3fcdfd1c741a3e62028814db68e10947b29
GodDB <saqwzx002@naver.com>
2021-08-31 20:59:04 +0900

    백준 1759 문제 풀이 완료

---
+++ src/Baekjoon_1759.kt
@@ -0,0 +1,51 @@
+import java.util.*
+
+private val vowelList: List<Char> = listOf('a', 'e', 'i', 'o', 'u')
+
+fun main() {
+
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextInt()
+    val m = scan.nextInt()
+    val arr = mutableListOf<Char>()
+    val result = MutableList(n) { 'a' }
+    val visitList = MutableList(m) { false }
+    for (i in 0 until m) {
+        arr.add(scan.next().first())
+    }
+    arr.sort()
+    backTracking(0, 0, arr, result, visitList, n, m)
+}
+
+private fun backTracking(
+    startIndex: Int,
+    prevIndex: Int,
+    arr: MutableList<Char>,
+    result: MutableList<Char>,
+    visitList: MutableList<Boolean>,
+    n: Int,
+    m: Int
+) {
+    if (startIndex == n) {
+        var vowelCnt = 0
+        var consonantCnt = 0
+        result.forEach { value ->
+            if (vowelList.contains(value)) vowelCnt++
+            else consonantCnt++
+        }
+
+        if (vowelCnt >= 1 && consonantCnt >= 2) {
+            println(result.joinToString(""))
+        }
+        return
+    }
+
+    for (i in prevIndex until m) {
+        if (visitList[i]) continue
+        visitList[i] = true
+        result[startIndex] = arr[i]
+        backTracking(startIndex + 1, i + 1, arr, result, visitList, n, m)
+        visitList[i] = false
+    }
+}

249f5ecd5057d4a106fa1d21c8f36dc8df27f842
GodDB <saqwzx002@naver.com>
2021-09-02 21:08:25 +0900

    백준 2239 문제 풀이 완료

--- src/Baekjoon_2239.kt
+++ src/Baekjoon_2239.kt
@@ -1,25 +1,7 @@
 import java.util.*
+import kotlin.system.exitProcess
 
 /** https://www.acmicpc.net/problem/2239 */
-
-private val cx2 = listOf(-1, 0, 1, 0)
-private val cy2 = listOf(0, -1, 0, 1)
-
-private var result: List<List<Int>>? = null
-    set(value) {
-        if(field == null) field = value
-
-        field?.forEachIndexed { i, list ->
-            list.forEachIndexed { j, itemValue ->
-                if(value!![i][j] > itemValue ) return
-                else {
-                    field = value
-                    return
-                }
-            }
-        }
-    }
-
 fun main() {
 
     val scan = Scanner(System.`in`)
@@ -33,70 +15,58 @@ fun main() {
             }
         }
     }
-
-    for (i in arr.indices) {
-        for (j in arr[i].indices) {
-            if (arr[i][j] != 0) {
-                val newArr = arr.toMutableList()
-                backTracking(i, j, newArr)
-                result = newArr
-            }
-        }
-    }
-
-    result?.forEach {
-        println(it.joinToString(""))
-    }
+    backTracking(0, 0, arr)
 }
 
 fun backTracking(i: Int, j: Int, arr: MutableList<MutableList<Int>>) {
+    if (j == 9) {
+        backTracking(i + 1, 0, arr)
+        return
+    }
+    if (i == 9) {
+        arr.forEach { println(it.joinToString("")) }
+        exitProcess(0)
+    }
 
-    for (z in cx2.indices) {
-        val newX = i + cx2[z]
-        val newY = j + cy2[z]
+    for (value in 1..9) {
 
-        //범위를 벗어났는가?
-        if (newX < 0 || newX > 8 || newY < 0 || newY > 8) continue
         //값이 지정되어 있는가?
-        if (arr[newX][newY] != 0) continue
-
-        for (value in 1..9) {
-            var isContinue = false
-
-            //열에 중복되는 숫자가 있는가?
-            for(q in 0 until 9) {
-                if (arr[newX][q] == value) {
-                    isContinue = true
-                    break
-                }
-            }
-            if(isContinue) continue
-
-            //행에 중복되는 숫자가 있는가?
-            for (q in 0 until 9) {
-                if (arr[q][newY] == value) {
-                    isContinue = true
-                    break
-                }
-            }
-            if(isContinue) continue
-
-            // 3x3 안에 동일한 숫자가 있는가?
-            for (q in (newX / 3) * 3 .. ((newX / 3) * 3) + 2) {
-                if(isContinue) break
-                for (k in (newY / 3) * 3 .. ((newY / 3) * 3) + 2) {
-                    if (arr[q][k] == value) {
-                        isContinue = true
-                        break
-                    }
-                }
-            }
-
-            if(isContinue) continue
-            arr[newX][newY] = value
-            break
+        if (arr[i][j] != 0) {
+            backTracking(i, j + 1, arr)
+            return
         }
 
-        backTracking(newX, newY, arr)
+        if (check(i, j, arr, value)) {
+            arr[i][j] = value
+            backTracking(i, j + 1, arr)
+            arr[i][j] = 0
+        }
     }
+}
+
+private fun check(i: Int, j: Int, arr: List<List<Int>>, value: Int): Boolean {
+    //열에 중복되는 숫자가 있는가?
+    for (q in 0 until 9) {
+        if (arr[i][q] == value) {
+            return false
+        }
+    }
+
+    //행에 중복되는 숫자가 있는가?
+    for (q in 0 until 9) {
+        if (arr[q][j] == value) {
+            return false
+        }
+    }
+
+    // 3x3 안에 동일한 숫자가 있는가?
+    for (q in (i / 3) * 3..((i / 3) * 3) + 2) {
+        for (k in (j / 3) * 3..((j / 3) * 3) + 2) {
+            if (arr[q][k] == value) {
+                return false
+            }
+        }
+    }
+
+    return true
 }

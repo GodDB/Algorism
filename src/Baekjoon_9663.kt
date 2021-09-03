eacd398516308d0c94207ffecb34a7428346e259
dongbeombae <dongbeom_bae@mycelebs.com>
2021-09-03 18:46:32 +0900

    백준 9663 문제 풀이 중

---
+++ src/Baekjoon_9663.kt
@@ -0,0 +1,41 @@
+import java.util.*
+import kotlin.math.abs
+
+/** https://www.acmicpc.net/problem/9663 */
+
+private var resultSum: Int = 0
+
+fun main() {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextInt()
+    val arr = MutableList(n) { 0 }
+
+    backTracking1(0, n, arr)
+    println("${resultSum}")
+}
+
+private fun backTracking1(x: Int, n: Int, arr: MutableList<Int>) {
+    if (x == n) {
+        resultSum++
+        return
+    }
+
+    for (value in 0 until n) {
+        if (check(arr, x, value)) {
+            arr[x] = value
+            backTracking1(x + 1, n, arr)
+        }
+    }
+}
+
+private fun check(arr: MutableList<Int>, x: Int, value: Int): Boolean {
+    for (i in 0 until x) {
+        // 대각선 검증
+        if (abs(arr[i] - i) == abs(value - x)) return false
+
+        // 열 검증
+        if (arr[i] == value) return false
+    }
+    return true
+}

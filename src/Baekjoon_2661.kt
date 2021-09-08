9cb06e4404a1561541e183e628aeebf22d0f115c
dongbeombae <dongbeom_bae@mycelebs.com>
2021-09-08 17:58:18 +0900

    백준 2661 문제 풀이 완료

--- src/Baekjoon_2661.kt
+++ src/Baekjoon_2661.kt
@@ -13,9 +13,9 @@ fun main() {
 }
 
 private fun backTracking(sum: String, arr: IntArray, n: Int) {
-    if(!check(sum, n)) return
+    if (!check(sum)) return
 
-    if(sum.length == n) {
+    if (sum.length == n) {
         println(sum)
         exitProcess(0)
     }
@@ -25,15 +25,17 @@ private fun backTracking(sum: String, arr: IntArray, n: Int) {
     }
 }
 
-private fun check(sum: String, n : Int): Boolean {
-    if(n == 1) return true
-
-    var prevValue = ""
-    for (i in 0 until sum.length - 1) {
-        if(sum[i] == sum[i+1]) return false
-        if(i%2 != 0) continue
-        if(prevValue == "${sum[i]}${sum[i+1]}") return false
-        prevValue = "${sum[i]}${sum[i+1]}"
+private fun check(sum: String): Boolean {
+    for (step in 1 until sum.length / 2 + 1) {
+        for (i in 0 until step) {
+            var prevValue = ""
+            for (j in sum.indices step step) {
+                if (sum.length < j + i + step) break
+                val curValue = sum.substring(j + i, j + i + step)
+                if (prevValue == curValue) return false
+                else prevValue = curValue
+            }
+        }
     }
     return true
 }
